efbcb446705b4e096c8c0ba71f4e62ace1ab499b
nno3onn <nno3onn@naver.com>
2025-06-26 23:24:42 +0900

    feat: setting NextAuth.js

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,1 +1,24 @@
- 
+import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+import Kakao from "next-auth/providers/kakao";
+
+export const authOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.AUTH_GITHUB_ID!,
+      clientSecret: process.env.AUTH_GITHUB_SECRET!,
+    }),
+    Kakao({
+      clientId: process.env.AUTH_KAKAO_ID!,
+      clientSecret: process.env.AUTH_KAKAO_SECRET!,
+    }),
+  ],
+};
+
+const handler = NextAuth(authOptions);
+
+export const GET = handler;
+export const POST = handler;
+
+// 캐싱 문제 방지용 옵션
+export const dynamic = "force-dynamic";
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,27 @@
-export default function Home() {
-  return <div></div>;
+"use client";
+
+import { useSession, signIn, signOut } from "next-auth/react";
+
+export default function Page() {
+  const { data: session, status } = useSession();
+
+  if (status === "loading") return <p>Loading...</p>;
+
+  return (
+    <div>
+      {status === "authenticated" ? (
+        <>
+          <p>Welcome, {session?.user?.name}</p>
+          <button onClick={() => signOut()}>Sign Out</button>
+        </>
+      ) : (
+        <>
+          <p>Not signed in</p>
+          <button onClick={() => signIn("github")}>Sign In with GitHub</button>
+          <br></br>
+          <button onClick={() => signIn("kakao")}>Sign In with Kakao</button>
+        </>
+      )}
+    </div>
+  );
 }
